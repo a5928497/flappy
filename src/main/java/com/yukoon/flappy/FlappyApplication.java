98c7bd8e3d7c9694f65552431e0659477596bab6
a5928497 <61722615@qq.com>
2018-08-10 17:30:53 +0800

    ready

---
+++ src/main/java/com/yukoon/flappy/FlappyApplication.java
@@ -0,0 +1,12 @@
+package com.yukoon.flappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlappyApplication.class, args);
+	}
+}
